8ac7b8d5929b04dab02b749f8482f1ede5323939
Santhosh Devale <santhosh.rao@egovernments.org>
2026-03-22 09:58:21 +0530

    updated the api path in env file

--- src/api.js
+++ src/api.js
@@ -1,14 +1,17 @@
 export async function saveCondition(data) {
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/save-condition`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(data)
-    }
-  );
+  const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+
+  if (!BASE_URL) {
+    throw new Error("API URL is missing 🚨");
+  }
+
+  const response = await fetch(`${BASE_URL}/save-condition`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  });
 
   return await response.json();
 }
